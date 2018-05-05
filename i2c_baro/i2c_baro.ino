93b8852e72c0e39641d72fe3a5aff3d4d144cb34
SL-Gremory <evanta1@me.com>
2018-05-05 14:09:28 -0700

    Updated with code from online, commented out

--- i2c_baro/i2c_baro.ino
+++ i2c_baro/i2c_baro.ino
@@ -1,9 +1,17 @@
 #include <Wire.h>
+//#include <MS5xxx.h>
+
+//MS5xxx sensor(&Wire);
 
 void setup() 
 {
   Wire.begin();
   Serial.begin(9600);
+  /*if(sensor.connect()>0) {
+    Serial.println("Error connecting...");
+    delay(500);
+    setup();
+  }*/
 }
 
 void loop() 
@@ -11,7 +19,7 @@ void loop()
   byte firstbyte=0;
   byte secondbyte=0;
   byte thirdbyte=0;
-  int numbyte=0;
+  int numbyte=1;
   int bytesavailable=0;
   
   // read 3 bytes, from address 0xEE
@@ -21,7 +29,6 @@ void loop()
   Serial.println(bytesavailable, DEC); //print available bytes
   
   while(Wire.read() && numbyte <= 3) {
-    numbyte++;
     if(numbyte == 1){
       firstbyte = Wire.receive();
     }
@@ -37,6 +44,7 @@ void loop()
       thirdbyte = 0;
       numbyte = 4;
     }
+    numbyte++;
   }
   
   numbyte = 0; //reset numbyte to zero
@@ -49,3 +57,28 @@ void loop()
   
   delay(2000);
 }
+/*
+void loop() {
+  sensor.ReadProm();
+  sensor.Readout();
+  Serial.print("Temperature [0.01 C]: ");
+  Serial.println(sensor.GetTemp());
+  Serial.print("Pressure [Pa]: ");
+  Serial.println(sensor.GetPres());
+  test_crc();
+  Serial.println("---");
+  delay(500);
+}
+
+void test_crc() {
+  sensor.ReadProm();
+  sensor.Readout(); 
+  Serial.print("CRC=0x");
+  Serial.print(sensor.Calc_CRC4(), HEX);
+    Serial.print(" (should be 0x");
+  Serial.print(sensor.Read_CRC4(), HEX);
+  Serial.print(")\n");
+  Serial.print("Test Code CRC=0x");
+  Serial.print(sensor.CRCcodeTest(), HEX);
+  Serial.println(" (should be 0xB)");
+}*/
