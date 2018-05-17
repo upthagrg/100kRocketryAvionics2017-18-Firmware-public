e3db895ee9f7c32737075fd9b0db81081cfb0f6d
hoffmross <rossghoffman4@gmail.com>
2018-05-16 17:15:49 -0700

    changed channel to channel 7 and attempted to get remote channel changing without luck, although the structure of code remains in the comments

--- IS_rx/IS_rx.ino
+++ IS_rx/IS_rx.ino
@@ -19,8 +19,11 @@
 
 const byte HC12RxdPin = 4;                  // Recieve Pin on HC12
 const byte HC12TxdPin = 5;                  // Transmit Pin on HC12
+const byte HC12setPin = 6;                  // Set Pin on HC12
 const int  switch1 = 3;                     // Switch1 pin
 bool       sw1 = 0;                         // Switch1 state
+String     readBuffer = "";                 // HC12 buffer
+byte       incomingByte;                    // Incoming byte to HC12
 
 SoftwareSerial HC12(HC12TxdPin,HC12RxdPin); // Create Software Serial Port
 
@@ -33,15 +36,34 @@ void setup() {
   pinMode(light_power, OUTPUT);
   pinMode(light_safe, OUTPUT);
   pinMode(Buzzer, OUTPUT);
+  pinMode(HC12setPin, OUTPUT);
 
-  digitalWrite(light_power, HIGH);          //when powered on
-  digitalWrite(light_safe, HIGH);          //when powered on
-  startup();                                //startup tone
+  digitalWrite(light_power, HIGH);     //when powered on
+  digitalWrite(light_safe, HIGH);      //when powered on
+  
+  //TRANSCEIVER CONFIGURATION---------------------------------------------------------------------
+  //Example: Send “AT+B38400” to module, and the module returns “OK+B19200”.
+  //3. AT+Cxxxx – Change wireless communication channel, from 001 to 100.
+  //Default: Channel 001, with working frequency of 433.4MHz. Each next channel is 400KHz higher.
+  //Example: If we want to set the module to channel 006, we need to send “AT+C006” command to the 
+  //module, and the module will return “OK+C006”. The new working frequency will be 435.4MHz.
+
+  //Channel 7 = 436.2MHz, use channel 2 433.8 if worried about range
+  
+  digitalWrite(HC12setPin, LOW);       // Set HC-12 into AT Command mode
+  delay(200);                          // Wait for the HC-12 to enter AT Command mode
+  HC12.print("AT+C007");               // Send AT Command to HC-12
+  delay(200);
+  digitalWrite(HC12setPin, HIGH);      // Exit AT Command mode
+  
+  //---------------------------------------------------------------------------------------------
+  
+  startup();                           //startup tone
+    
 }
 
 void loop() {
 
-  
   //checks safe light
   if(digitalRead(switch1) == HIGH){
     digitalWrite(light_safe, LOW);
@@ -51,22 +73,96 @@ void loop() {
     digitalWrite(light_safe, HIGH);
     sw1 = 0;
   }
-    
-   
-  if(sw1 == 1){ //checks that switch turned
-    if(HC12.available()){    
-      int input = HC12.parseInt();  //read serial input and convert to integer (-32,768 to 32,767)    
+
+  //radio link handler-----------------  
+  if(HC12.available()){    
+    int input = HC12.parseInt();       //read serial input and convert to integer (-32,768 to 32,767)    
+ 
+    //LAUNCH CONDITION
+    if(sw1 == 1){                      //checks that switch turned
       if(input == 15018){              //if on code is received
         digitalWrite(Relay, HIGH);     //Fire Rocket
-        delay(500);                   //Shorts E-Match for 1 second
+        delay(500);                    //Shorts E-Match for 1 second
         digitalWrite(Relay, LOW);      //Turns relay off
         input = 0;                     //Resets input variable   
       }
     }
-    HC12.flush();                   //clear the serial buffer for unwanted inputs     
-  
-    delay(20);                          //small delay to improve serial communication
+
+    //COMM LINK STATUS
+    if(input == 4444){
+      comm_beep();                     //comm beep pattern
+      input = 0;                       //resets input value
+    }
+
+    if(input == 5555){                 //arm beep upon arm code receive
+      tone(Buzzer, 4000);
+    }
+    else noTone;                       //turns buzzer off then gets a command other than armed              
+
+    /*if(input == 7777){                 //change frequency channel
+      delay(20);
+      incomingByte = HC12.read();      //store each incoming byte 
+      readBuffer += char(incomingByte);//add each byte to buffer
+      digitalWrite(HC12setPin, LOW);   //Set HC-12 into AT Command mode
+      delay(100);                      //Wait for the HC-12 to enter AT Command mode
+      HC12.print(readBuffer);          //sends AT command to HC12
+      delay(200);
+      digitalWrite(HC12setPin, HIGH);  //Exit AT Command mode
+      input = 0;                       //resets input value
+    }*/
+    
+    HC12.flush();                      //clear the serial buffer for unwanted inputs     
+    delay(20);                         //small delay to improve serial communication
   }
+}
+
+void comm_beep(){
+
+  digitalWrite(light_comm, HIGH);  //flashes comm yellow light
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  digitalWrite(light_comm, LOW);
+  delay(500);
+
+  digitalWrite(light_comm, HIGH);  //flashes comm yellow light
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  delay(50);
+  tone(Buzzer, 4800);
+  delay(50);
+  noTone(Buzzer);
+  digitalWrite(light_comm, LOW);
+  delay(150);
 }
 
 void startup(){
--- IS_tx/IS_tx.ino
+++ IS_tx/IS_tx.ino
@@ -1,34 +1,39 @@
 #include <SoftwareSerial.h>
+#include <SimpleTimer.h>
 
 const int launch_code = 15018; //launch code
-const byte HC12RxdPin = 4; // Recieve Pin on HC12
-const byte HC12TxdPin = 5; // Transmit Pin on HC12
-const int switch1 = 8;  //comm enable switch
-const int switch2 = 9;  //turn key
-const int switch3 = 10; //button (active-low)
-const int switch4 = 11; //arm
-const int relay8  = 3;  //comm enable light
-const int relay7  = 2;  //buzzer alarm
-const int relay6  = 1;  //arm light
-const int relay5  = 0;  //power light
-const int relay4  = 12; //status light
-const int relay3  = 13; //RF power amplifier
+const byte HC12RxdPin = 4;     // Recieve Pin on HC12
+const byte HC12TxdPin = 5;     // Transmit Pin on HC12
+const byte HC12setPin = 6;     // Set Pin on HC12
+const int switch1 = 8;         //comm enable switch
+const int switch2 = 9;         //turn key
+const int switch3 = 10;        //button (active-low)
+const int switch4 = 11;        //arm
+const int relay8  = 3;         //comm enable light
+const int relay7  = 2;         //buzzer alarm
+const int relay6  = 1;         //arm light
+const int relay5  = 0;         //power light
+const int relay4  = 12;        //status light
+const int relay3  = 13;        //RF power amplifier
 
-bool comm_en      = 0;  //switch1
-bool turn_key     = 0;  //switch2
-bool send_it      = 1;  //switch3 (high because active low)
-bool arm          = 0;  //swtich4
-bool comm_light   = 0;  //relay8
-bool buzzer       = 0;  //relay7
-bool arm_light    = 0;  //relay6
-bool power_light  = 0;  //relay5
-bool status_light = 0;  //relay4 blinks when pings, shows error
-bool rf_pwr_amp   = 0;  //relay3
+bool comm_en      = 0;         //switch1
+bool turn_key     = 0;         //switch2
+bool send_it      = 1;         //switch3 (high because active low)
+bool arm          = 0;         //swtich4
+bool comm_light   = 0;         //relay8
+bool buzzer       = 0;         //relay7
+bool arm_light    = 0;         //relay6
+bool power_light  = 0;         //relay5
+bool status_light = 0;         //relay4 blinks when pings, shows error
+bool rf_pwr_amp   = 0;         //relay3
 //all relays active low
 
 SoftwareSerial HC12(HC12TxdPin,HC12RxdPin); // Create Software Serial Port
 
-bool stay = 0; //keeps the lower setup loop running if start up conditions are not met
+int  arm_count    = 0;  //arm ping counter
+bool beep_once    = 0;  //only beeps on initial key turn    
+bool stay         = 0;  //keeps the lower setup loop running if start up conditions are not met
+SimpleTimer timer;      //creates timer object
 
 void setup() {
   
@@ -43,6 +48,7 @@ void setup() {
   pinMode(switch2, INPUT);
   pinMode(switch3, INPUT);
   pinMode(switch4, INPUT);
+  pinMode(HC12setPin, OUTPUT);
 
   digitalWrite(relay3, HIGH);
   digitalWrite(relay4, HIGH);
@@ -103,36 +109,111 @@ void setup() {
     HC12.begin(9600);     // Open serial port to HC12 after initial conditions checked
     
   }while(stay == 1);      //while the error persists 
+
+  timer.setInterval(15000, ping); //calls ping every 15 seconds
+
+  //TRANSCEIVER CONFIGURATION---------------------------------------------------------------------
+  //Example: Send “AT+B38400” to module, and the module returns “OK+B19200”.
+  //3. AT+Cxxxx – Change wireless communication channel, from 001 to 100.
+  //Default: Channel 001, with working frequency of 433.4MHz. Each next channel is 400KHz higher.
+  //Example: If we want to set the module to channel 006, we need to send “AT+C006” command to the 
+  //module, and the module will return “OK+C006”. The new working frequency will be 435.4MHz.
+
+  //Channel 7 = 436.2MHz, use channel 2 433.8 if worried about range
+  //digitalWrite(relay3, LOW);           //turns on power amp
+  //digitalWrite(relay4, LOW);           //flashes status light on
+  //delay(500);
+  
+  //HC12.print(7777);                    // sends change channel command to RX box
+  //delay(20);
+  //HC12.print("AT+C007");               // sends channel code to RX box
+  //delay(200);
+  
+  digitalWrite(HC12setPin, LOW);       // Set HC-12 into AT Command mode
+  delay(200);                          // Wait for the HC-12 to enter AT Command mode
+  HC12.print("AT+C007");               // Send AT Command to HC-12
+  delay(200);
+  digitalWrite(HC12setPin, HIGH);      // Exit AT Command mode
+  
+  //digitalWrite(relay3, HIGH);          //turns off power amp
+  //digitalWrite(relay4, HIGH);          //flashes status light on
+  //---------------------------------------------------------------------------------------------
+  
 }  
 
 void loop() {
   
-  //reads states of switches
-  if(digitalRead(switch1) == 1){
+  //reads states of switches------------------------
+  if(digitalRead(switch1) == 1){                    //reads comm enable switch
     comm_en = 1;
-    digitalWrite(relay3, LOW); //turns on power amp
+    digitalWrite(relay3, LOW);                      //turns on power amp
+    timer.run();                                    //runs 15 second timer
   }
   else{
     comm_en = 0;
-    digitalWrite(relay3, HIGH); //turns off power amp
+    digitalWrite(relay3, HIGH);                     //turns off power amp
   }
-  if(digitalRead(switch2) == 1) turn_key = 1;
-  else turn_key = 0;
-  if(digitalRead(switch4) == 1) arm = 1;
-  else arm = 0;
+  
+  if(digitalRead(switch2) == 1){                    //reads turn key position
+    turn_key = 1;
+    delay(100);                                     //turn key debounce delay
+    if(beep_once == 0){                             //only beeps on initial key turn
+      digitalWrite(relay7, LOW);                    //turns buzzer on
+      delay(1000);                                  //sounds for half a second
+      digitalWrite(relay7, HIGH);                   //turns buzzer off 
+      beep_once = 1;
+    }        
+  }
+  else{
+    turn_key = 0;
+    beep_once = 0;
+  }
+  
+  if(digitalRead(switch4) == 1){                    //reads arm switch position
+    arm = 1;
+    delay(250);                                        
+    if(arm_count < 4){
+      digitalWrite(relay4, LOW);                    //flashes status light on
+      HC12.println(5555);                           //sends arm code 4 times
+      arm_count++;                                  //increment arm count by 1
+      delay(40);                                    //small delay between transmissions
+      digitalWrite(relay4, HIGH);                   //flashes the status light off
+    }
+  }
+  else{
+    arm = 0;                                        //resets arm state
+    arm_count = 0;                                  //resets arm count
+  }
 
   check_errors(); //errors
   update_LEDS();  //lights
   check_launch(); //senditt
+   
 }
 
 void check_launch(){
+  
   if(comm_en == 1 && turn_key == 1){ //comm_en and key turned
     check_errors();                  //checks errors again for good measure
     if(digitalRead(switch3) == 0){   //when red button pressed
       launch();                      //calls launch function
     }
   }
+  
+}
+
+void ping(){
+  
+  //comm link pings if comm_en switch is turned and arm switch is off
+  if(comm_en == 1 && arm == 0){
+    digitalWrite(relay4, LOW);       //flashes status light
+    HC12.println("KI7NXS");          //sends callsign every 15 seconds as well
+    delay(100);
+    HC12.println(4444);              //sends comm link code
+    delay(150);                      //makes light visable
+    digitalWrite(relay4, HIGH);
+  }
+       
 }
 
 void check_errors(){
@@ -141,44 +222,49 @@ void check_errors(){
     //If Dallas tries to turn the key but he didn’t already arm the button switch
     //throw the status light and lockout until the key is turned back to the safe state
     if(turn_key == 1 && arm == 0){
-      digitalWrite(relay7, LOW); //buzzer sounds
-      status_light = 1;          //sets status light state high
-      digitalWrite(relay4, LOW); //turns on status light
-      stay = 1;                  //locks out user, forces reset to continue
+      digitalWrite(relay7, LOW);    //buzzer sounds
+      status_light = 1;             //sets status light state high
+      digitalWrite(relay4, LOW);    //turns on status light
+      stay = 1;                     //locks out user, forces reset to continue
     }
     //If someone tries to arm without comm enabled, throw an error
     if(comm_en == 0 && arm == 1){
-      digitalWrite(relay7, LOW); //buzzer sounds
-      status_light = 1;          //sets status light state high
-      digitalWrite(relay4, LOW); //turns on status light
-      stay = 1;                  //locks out user, forces reset to continue
+      digitalWrite(relay7, LOW);    //buzzer sounds
+      status_light = 1;             //sets status light state high
+      digitalWrite(relay4, LOW);    //turns on status light
+      stay = 1;                     //locks out user, forces reset to continue
     }
     
   }while(stay == 1);
+  
 }
 
 void update_LEDS(){
-  if(comm_en == 1) digitalWrite(relay8, LOW); //turns on comm enable light
+  
+  if(comm_en == 1) digitalWrite(relay8, LOW);      //turns on comm enable light
   else digitalWrite(relay8, HIGH);
-  if(arm == 1) digitalWrite(relay6, LOW); //turns on arm light
+  if(arm == 1) digitalWrite(relay6, LOW);          //turns on arm light
   else digitalWrite(relay6, HIGH);
   if(status_light == 1) digitalWrite(relay4, LOW); //turns on status light
   else digitalWrite(relay4, HIGH);
+  
 }
 
 void launch(){
-  digitalWrite(relay7, LOW); //turns buzzer on //DEBUG
-  delay(250);
-  digitalWrite(relay7, HIGH); //turns buzzer off
-
-  HC12.println(launch_code); //sends launch code
   
-  //Serial.begin(9600);           //debug
-  //Serial.println(launch_code); 
+  //digitalWrite(relay7, LOW);  //turns buzzer on 
+  digitalWrite(relay4, LOW);  //flashes status light
+  
+  HC12.println(launch_code);  //sends launch code
+  
+  delay(150);
+  digitalWrite(relay4, HIGH); //turns status light off
+  //digitalWrite(relay7, HIGH); //turns buzzer off
     
 }
 
 void full_reset(){
+  
   //resets all global variables
   comm_en      = 0;  //switch1
   turn_key     = 0;  //switch2
@@ -198,6 +284,8 @@ void full_reset(){
   digitalWrite(relay6, HIGH);
   digitalWrite(relay7, HIGH);
   digitalWrite(relay8, HIGH); 
+
 }
+
 
 
